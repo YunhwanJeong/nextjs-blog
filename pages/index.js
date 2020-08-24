22bb154d6cc182a337e95e6e4b1381ee1270655b
aiden-jung <jimmy53120488@gmail.com>
2020-08-24 16:39:03 +0900

    FIX: 자기소개 수정

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
+        <p>Hello! I'm Aiden. I like to make things with JS.</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{" "}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
